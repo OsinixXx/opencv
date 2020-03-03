87644b8dca1e9a9b76c7dfefdaac64d9e3563b53
OsinixXx <61703961+OsinixXx@users.noreply.github.com>
2020-03-03 23:35:37 +0100

    Add files via upload

---
+++ untitled0.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  3 22:20:20 2020
+
+@author: osini
+"""
+
+
+
+import cv2 as cv
+
+img = cv.imread('peguin.jpg', cv.IMREAD_GRAYSCALE);
+cv.imshow("img", img);
+
+
+
+
